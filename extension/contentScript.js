09976b18c336fcc7003d78e5da8c6c739bf8437f
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2016-08-07 13:48:16 -0500

    registering seen images

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,20 +1,7 @@
 let prevLocation = {}
-let lastSession = null
-let currentSession = {
-  furthestScroll: 0,
-  seenExamples: [],
-  unseenExamples: []
-}
-
-// const observerConfig = {
-//   childList: true
-// }
-//
-// const observer = new MutationObserver(mutations => {
-//   for (let m of mutations) {
-//     console.log(m)
-//   }
-// })
+let examplesSeen = {}
+let lastScrollPosition = 0
+const scrolltheshold = 50
 
 const pinRegexp = /^\/pin\/([^\/]*)\/$/
 const searcnRegexp = /^\/search\/pins\/$/
@@ -26,142 +13,82 @@ const boardRegexp = /^\/([^\/]*)\/([^\/]*)\/$/
 
 setInterval(() => {
   if (window.location.href !== prevLocation.href) {
+    examplesSeen = {}
+
     if (pinRegexp.test(window.location.pathname)) {
       const pinId = pinRegexp.exec(window.location.pathname)[1]
-
-      lastSession = Object.assign({}, currentSession)
-      currentSession = {
-        type: 'image',
-        image: {/* TODO: construct image object */},
-        furthestScroll: 0,
-        seenExamples: [],
-        unseenExamples: []
-      }
-
-      // processLoadedExamples(document.querySelectorAll('.pinImageWrapper'))
-
+      // send new history to server
       console.log(`pin ${pinId}`)
-      console.log(currentSession)
+      const closeUp = document.querySelector('.Closeup')
+
+      if (closeUp) {
+        closeUp.addEventListener('scroll', () => {
+          const currentScrollPosition = closeUp.scrollTop
+
+          if (Math.abs(currentScrollPosition - lastScrollPosition) > scrolltheshold) {
+            registerExamplesSeen(document.querySelectorAll('.closeupBottom .pinImageWrapper'))
+            lastScrollPosition = currentScrollPosition
+          }
+        }, false)
+      }
     } else if (searcnRegexp.test(window.location.pathname)) {
       const searchQuery = window.decodeURIComponent(searchQueryRegexp.exec(window.location.search)[1])
-
-      if (lastSession && lastSession.type === 'keyword' && lastSession.query === searchQuery) {
-        currentSession = Object.assign({}, lastSession)
-
-        console.log(`(continue) search for ${searchQuery}`)
-      } else {
-        currentSession = {
-          type: 'keyword',
-          query: searchQuery,
-          furthestScroll: 0,
-          seenExamples: [],
-          unseenExamples: []
-        }
-
-        console.log(`search for ${searchQuery}`)
-        processLoadedExamples(document.querySelectorAll('.pinImageWrapper'))
-        updateUnseenExamples()
-      }
-
-      lastSession = null
-      console.log(currentSession)
+      console.log(`search for ${searchQuery}`)
+      // send new history to server
     } else if (categoriesRegexp.test(window.location.pathname)) {
-      // const category = categoriesRegexp.exec(window.location.pathname)[1]
-      // unseenExamples = []
-      // seenExamples = []
-      // furthestScroll = 0
-      //
-      // observer.disconnect()
-      // console.log(`category ${category}`)
-      // processLoadedExamples(document.querySelectorAll('.pinImageWrapper'))
-      // updateUnseenExamples()
-      // observer.observe(document.querySelector('.GridItems'), observerConfig)
+      const category = categoriesRegexp.exec(window.location.pathname)[1]
+      console.log(`category ${category}`)
+      // send new history to server
     } else if (topicsRegexp.test(window.location.pathname)) {
-      // const topic = topicsRegexp.exec(window.location.pathname)[1]
-      // unseenExamples = []
-      // seenExamples = []
-      // furthestScroll = 0
-      //
-      // observer.disconnect()
-      // console.log(`topic for ${topic}`)
-      // processLoadedExamples(document.querySelectorAll('.pinImageWrapper'))
-      // updateUnseenExamples()
-      // observer.observe(document.querySelector('.GridItems'), observerConfig)
+      const topic = topicsRegexp.exec(window.location.pathname)[1]
+      console.log(`topic for ${topic}`)
+      // send new history to server
     } else if (newsRegexp.test(window.location.pathname)) {
-      // unseenExamples = []
-      // seenExamples = []
-      // furthestScroll = 0
-      //
-      // observer.disconnect()
-      // console.log('news')
-      // processLoadedExamples(document.querySelectorAll('.pinImageWrapper'))
-      // updateUnseenExamples()
-      // observer.observe(document.querySelector('.GridItems'), observerConfig)
+      console.log('news')
+      // send new history to server
     } else if (boardRegexp.test(window.location.pathname)) {
-      // const matches = boardRegexp.exec(window.location.pathname)
-      // const username = matches[1]
-      // const boardname = matches[2]
-      // unseenExamples = []
-      // seenExamples = []
-      // furthestScroll = 0
-      //
-      // observer.disconnect()
-      // console.log(`board ${boardname} by ${username}`)
-      // processLoadedExamples(document.querySelectorAll('.pinImageWrapper'))
-      // updateUnseenExamples()
-      // observer.observe(document.querySelector('.GridItems'), observerConfig)
+      const matches = boardRegexp.exec(window.location.pathname)
+      const username = matches[1]
+      const boardname = matches[2]
+      console.log(`board ${boardname} by ${username}`)
+      // send new history to server
     }
   }
 
   prevLocation = Object.assign({}, window.location)
 }, 100)
 
-const handleNewQuery = query => {
-  // send query to server
-  // for now just logging...
-  console.log(query)
+const registerExamplesSeen = (exampleElementsAvailable) => {
+  console.log(exampleElementsAvailable.length)
 
-  unseenExamples = []
-  seenExamples = []
-  furthestScroll = 0
+  for (let exampleElement of exampleElementsAvailable) {
+    const splitHref = exampleElement.href.split('/')
+    const exampleId = splitHref[splitHref.length - 2]
 
-  processLoadedExamples(document.querySelectorAll('.pinImageWrapper'))
+    if (examplesSeen[exampleId] === undefined) {
+      const example = {
+        id: exampleId,
+        url: exampleElement.querySelector('img').src,
+        boundingClientRect: exampleElement.getBoundingClientRect(),
+        node: exampleElement
+      }
 
-  const searchResultsWrapper = document.querySelector('.GridItems')
-  observer.disconnect()
-  observer.observe(searchResultsWrapper, observerConfig)
-}
-
-const processLoadedExamples = (exampleElements) => {
-  for (let exampleElement of exampleElements) {
-    const exampleHrefParts = exampleElement.href.split('/')
-
-    currentSession.unseenExamples.push({
-      id: exampleHrefParts[exampleHrefParts.length - 2],
-      url: exampleElement.querySelector('img').src,
-      boundingClientRect: exampleElement.getBoundingClientRect()
-    })
-  }
-}
-
-const updateUnseenExamples = () => {
-  const currentScroll = window.scrollY
-  const unseen = []
-
-  if (currentScroll > currentSession.furthestScroll) {
-    for (let example of currentSession.unseenExamples) {
-      if (window.innerHeight + currentScroll - example.boundingClientRect.bottom > 0) {
-        currentSession.seenExamples.push(Object.assign({}, example))
-      } else {
-        unseen.push(Object.assign({}, example))
+      if ((example.boundingClientRect.bottom < window.innerHeight) &&
+          (example.boundingClientRect.bottom > 0)) {
+        examplesSeen[exampleId] = example
       }
     }
-
-    currentSession.unseenExamples = [...unseen]
-    // send 'seen' to server
-
-    currentSession.furthestScroll = currentScroll
   }
+
+  console.log({length: Object.keys(examplesSeen).length, objects: examplesSeen})
 }
 
-window.addEventListener('scroll', updateUnseenExamples, false)
+window.addEventListener('scroll', () => {
+  const currentScrollPosition = window.scrollY
+
+  if (Math.abs(currentScrollPosition - lastScrollPosition) > scrolltheshold) {
+    registerExamplesSeen(document.querySelectorAll('.mainContainer .pinImageWrapper'))
+
+    lastScrollPosition = currentScrollPosition
+  }
+}, false)
