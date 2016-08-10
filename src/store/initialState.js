cb9a087e96038ef7bd8a4c1c7a74edc1251e4dfe
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2016-08-10 14:37:49 -0500

    removed some old code

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -1,10 +1,4 @@
 export default {
-  popup: {
-    isShown: false,
-    timeoutId: null,
-    currentIndex: null,
-    historyId: null
-  },
   histories: {
     items: []
   }
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,7 @@
 import {combineReducers} from 'redux'
-import popup from './popupReducer'
 import histories from './historiesReducer'
 
 const rootReducer = combineReducers({
-  popup,
   histories
 })
 
